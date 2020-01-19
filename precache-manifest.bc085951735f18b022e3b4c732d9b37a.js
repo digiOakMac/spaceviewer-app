5b1c388d6c81c06d459c0dc49dcfa9ef6b0be340
Oak Chantosa <o.chantosa@gmail.com>
2020-01-19 14:49:44 -0800

    Updates

---
+++ precache-manifest.bc085951735f18b022e3b4c732d9b37a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4140b0d998109fab217f9da67f0037c4",
+    "url": "/spaceviewer-app/index.html"
+  },
+  {
+    "revision": "711378afa12ae9430f26",
+    "url": "/spaceviewer-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c34588ff82219799e48a",
+    "url": "/spaceviewer-app/static/js/2.0c58d0a2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/spaceviewer-app/static/js/2.0c58d0a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "711378afa12ae9430f26",
+    "url": "/spaceviewer-app/static/js/main.fd74e373.chunk.js"
+  },
+  {
+    "revision": "1d8bf5a4956529f300c0",
+    "url": "/spaceviewer-app/static/js/runtime-main.c304b414.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spaceviewer-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
